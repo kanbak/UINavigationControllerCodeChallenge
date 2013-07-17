6a534e7460151cfcfe2ce25c6c8f31333f8f5f21
Umut Kanbak <uk@kanbak.net>
2013-07-16 21:38:54 -0500

    UITableView challenge 7-16-03
    got to half vay through version 1

---
+++ My Favorite Table Things/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  My Favorite Table Things
+//
+//  Created by Umut Kanbak on 7/16/13.
+//  Copyright (c) 2013 Umut Kanbak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ My Favorite Table Things/ViewController.m
@@ -0,0 +1,73 @@
+//
+//  ViewController.m
+//  My Favorite Table Things
+//
+//  Created by Umut Kanbak on 7/16/13.
+//  Copyright (c) 2013 Umut Kanbak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    __weak IBOutlet UITextField *myTextField;
+    
+    __weak IBOutlet UITableView *myTableView;
+    NSMutableArray *myArray;
+}
+- (IBAction)addButton:(id)sender;
+- (IBAction)removeButton:(id)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    myArray = [NSMutableArray arrayWithCapacity:10];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)addButton:(id)sender {
+    [myArray addObject:myTextField.text];
+    [myTableView reloadData];
+    [myTextField resignFirstResponder];
+}
+
+- (IBAction)removeButton:(id)sender {
+    [myArray removeLastObject];
+    [myTableView reloadData];
+    [myTextField resignFirstResponder];
+}
+
+#pragma mark UITableViewDataSource
+
+-(int)numberOfSectionsInTableView:(UITableView *)tableView;
+{
+    return 1;
+}
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"my identifier"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"my identifier"];
+    }
+    cell.textLabel.text = [myArray objectAtIndex:indexPath.row];
+    return cell;
+}
+
+-(int)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [myArray count];
+}
+
+
+@end
